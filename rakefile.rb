607e1daa4f51f39404d9a4859829c8ecfdf7b04c
n-fujita <n-fujita@daiichi-g.co.jp>
2014-02-13 16:28:16 +0900

    テストケース追加
    重複が起こるケースを追加

--- rakefile.rb
+++ rakefile.rb
@@ -6,7 +6,7 @@ require "./auto_selecter"
 def puts_sentences status, selected, overlap
   if status
     puts overlap
-    puts selected.map{|s| s.to_s}.join("\n")
+    puts selected.map{|s| s.to_s}.join(" / ")
   else
     puts "not detected."
   end
@@ -14,6 +14,8 @@ end
 
 task :default do
   rs = AutoSelector.new "testdata.txt"
-  puts_sentences(*(rs.auto_select [1001,1002], [3,5]))
+  puts_sentences(*(rs.auto_select [1001,1002], [3,3]))
   puts_sentences(*(rs.auto_select [1002,1004], [5,3]))
+  puts_sentences(*(rs.auto_select [1001,1002], [5,5]))
+  puts_sentences(*(rs.auto_select [1003,1004], [5,3]))
 end
